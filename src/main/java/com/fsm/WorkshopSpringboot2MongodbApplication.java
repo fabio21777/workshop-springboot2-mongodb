714add455b27b37f8b3fa023edc039ded6b03376
fabio21777 <fabiosouza21777@gmail.com>
2021-07-08 16:43:37 -0300

    first commit

---
+++ src/main/java/com/fsm/WorkshopSpringboot2MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.fsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot2MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot2MongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsm/WorkshopSpringboot2MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot2MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
