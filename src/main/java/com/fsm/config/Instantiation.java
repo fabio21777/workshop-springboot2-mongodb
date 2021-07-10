a6d175f1be27e08d13aeac520503226f35fa156c
fabio21777 <fabiosouza21777@gmail.com>
2021-07-10 17:53:19 -0300

    created entity Post with nested User

--- src/main/java/com/fsm/config/Instantiation.java
+++ src/main/java/com/fsm/config/Instantiation.java
@@ -1,30 +1,44 @@
 package com.fsm.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.fsm.domain.Post;
 import com.fsm.domain.User;
+import com.fsm.repository.PostRepository;
 import com.fsm.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 	@Autowired 
 	UserRepository userRepository;
-
+	@Autowired
+	PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
+		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 	}
 	
