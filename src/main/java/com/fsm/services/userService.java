24e2f9b4a17a71ba4dbeea97e780903c2fac2515
fabio21777 <fabiosouza21777@gmail.com>
2021-07-10 15:47:31 -0300

    getting a user by id

--- src/main/java/com/fsm/services/userService.java
+++ src/main/java/com/fsm/services/userService.java
@@ -1,12 +1,14 @@
 package com.fsm.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fsm.domain.User;
 import com.fsm.repository.UserRepository;
+import com.fsm.services.exception.ObjectNotFoundException;
 
 @Service
 public class userService {
@@ -15,5 +17,9 @@ public class userService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
