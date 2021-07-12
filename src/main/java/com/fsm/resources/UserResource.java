37d769eaefe9188a055d76469e9a6fa521d655a3
fabio21777 <fabiosouza21777@gmail.com>
2021-07-12 15:49:36 -0300

    endpoint to return the user posts

--- src/main/java/com/fsm/resources/UserResource.java
+++ src/main/java/com/fsm/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fsm.domain.Post;
 import com.fsm.domain.User;
 import com.fsm.dto.UserDTO;
 import com.fsm.services.userService;
@@ -59,6 +60,11 @@ public class UserResource {
 		user.setId(id);
 		user = userService.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>>findPosts(@PathVariable String id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }
