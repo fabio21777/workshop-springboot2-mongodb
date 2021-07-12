2816384049f7cd75e383c985a7f338574eccf32e
fabio21777 <fabiosouza21777@gmail.com>
2021-07-12 16:24:21 -0300

    Adding comments to posts

--- src/main/java/com/fsm/config/Instantiation.java
+++ src/main/java/com/fsm/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.fsm.domain.Post;
 import com.fsm.domain.User;
 import com.fsm.dto.AuthorDTO;
+import com.fsm.dto.CommentDTO;
 import com.fsm.repository.PostRepository;
 import com.fsm.repository.UserRepository;
 
@@ -42,6 +43,17 @@ public class Instantiation implements CommandLineRunner {
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		
 		
 	}
 	
--- src/main/java/com/fsm/domain/Post.java
+++ src/main/java/com/fsm/domain/Post.java
@@ -1,13 +1,16 @@
 package com.fsm.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fsm.dto.AuthorDTO;
+import com.fsm.dto.CommentDTO;
 
 @Document(collection = "post")
 public class Post implements Serializable {
@@ -20,7 +23,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
-
+	private List<CommentDTO> comments = new ArrayList<>();
 
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
@@ -71,6 +74,12 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
 
 	@Override
 	public int hashCode() {
