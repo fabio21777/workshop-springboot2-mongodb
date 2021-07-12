63a127901508c8336cd62677393b04e9a1520e25
fabio21777 <fabiosouza21777@gmail.com>
2021-07-12 15:20:36 -0300

    projection of author data with DTO

---
+++ src/main/java/com/fsm/dto/AuthorDTO.java
@@ -0,0 +1,33 @@
+package com.fsm.dto;
+
+import java.io.Serializable;
+
+import com.fsm.domain.User;
+
+public class AuthorDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String name;
+	
+	public AuthorDTO(){
+		
+	}
+	public AuthorDTO (User user){
+		id = user.getId();
+		name = user.getName();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
